b5e3861a654e3abd8c0cc948ea637d0f1d0bfdef
KampalaUdp <146834712+KampalaUdp@users.noreply.github.com>
2024-04-01 10:05:52 +0300

    Update masoltionsudp.sh

--- masoltionsudp.sh
+++ masoltionsudp.sh
@@ -22,10 +22,10 @@ PROTOCOL="udp"
 UDP_PORT=":50000"
 
 # OBFS
-OBFS="mayamax."
+OBFS="droidmax."
 
 # PASSWORDS
-PASSWORD="mayamax."
+PASSWORD="droidmax."
 
 # Basename of this script
 SCRIPT_NAME="$(basename "$0")"
