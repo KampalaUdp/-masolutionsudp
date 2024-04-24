b803a5e55285ba4e7643802ade390e3338dffd2e
KampalaUdp <146834712+KampalaUdp@users.noreply.github.com>
2024-04-24 04:56:47 +0300

    Update masoltionsudp.sh

--- masoltionsudp.sh
+++ masoltionsudp.sh
@@ -22,10 +22,10 @@ PROTOCOL="udp"
 UDP_PORT=":50000"
 
 # OBFS
-OBFS="april."
+OBFS="base."
 
 # PASSWORDS
-PASSWORD="april."
+PASSWORD="base."
 
 # Basename of this script
 SCRIPT_NAME="$(basename "$0")"
